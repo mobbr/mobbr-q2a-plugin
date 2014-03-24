17024df0505b0793200dfc3eee018ff079138c5b
Han Dijk <info@handijk.nl>
2014-03-24 09:00:59 +0100

    remove duplicate mobbr id resolver and fix the id base

--- qa-mobbr-layer.php
+++ qa-mobbr-layer.php
@@ -13,7 +13,7 @@
             else
             {
                 $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-                return  $protocol . $_SERVER['HTTP_HOST'] .  $_SERVER['PHP_SELF'] . '?qa=mobbr_';
+                return  qa_opt('site_url') . '?qa=mobbr_';
             }
         }
 
