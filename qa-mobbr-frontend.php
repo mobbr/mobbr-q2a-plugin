29b86c6ff492e31d52de7707aa5ee019ba600343
pupi1985 <pupi1985@bitbucket.org>
2014-12-06 13:11:55 -0300

    URL is now sent as a paramenter to the JS button generation function

--- qa-mobbr-frontend.php
+++ qa-mobbr-frontend.php
@@ -2,13 +2,14 @@
 
     class qa_mobbr_frontend {
 
-        static function get_html_button()
+        static function get_html_button($questionid = null)
         {
             require_once 'qa-mobbr.php';
 
             $buttontype = qa_mobbr::$buttontypes[qa_opt('mobbr_support_buttontype')];
             $currency = qa_opt('mobbr_support_currency');
-            $buttonhtml = '<script type="text/javascript">mobbr.button' . $buttontype. '(' . '"", "' . $currency . '");</script>';
+            $url = isset($questionid) ? qa_q_path($questionid, '', true) : '';
+            $buttonhtml = sprintf('<script type="text/javascript">mobbr.button%s("%s", "%s");</script>', $buttontype, $url, $currency);
             $html = qa_opt('mobbr_support_button_html');
             if (strpos($html, '{{button}}')) {
                 $html = str_replace('{{button}}', $buttonhtml, $html);
