a87dd8de60af2485a779db49de0645b81da23ed6
Patrick Savalle <patrick@mobbr.com>
2014-12-08 12:04:28 +0100

    Fixed URL in button's on list pages.

--- qa-mobbr-frontend.php
+++ qa-mobbr-frontend.php
@@ -2,13 +2,13 @@
 
     class qa_mobbr_frontend {
 
-        static function get_html_button($questionid = null)
+        static function get_html_button($questionid = null, $questiontitle = null )
         {
             require_once 'qa-mobbr.php';
 
             $buttontype = qa_mobbr::$buttontypes[qa_opt('mobbr_support_buttontype')];
             $currency = qa_opt('mobbr_support_currency');
-            $url = isset($questionid) ? qa_q_path($questionid, '', true) : '';
+            $url = isset($questionid) ? qa_q_path($questionid, $questiontitle, true) : '';
             $buttonhtml = sprintf('<script type="text/javascript">mobbr.button%s("%s", "%s");</script>', $buttontype, $url, $currency);
             $html = qa_opt('mobbr_support_button_html');
             if (strpos($html, '{{button}}')) {
